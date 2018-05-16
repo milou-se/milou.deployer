1a705665438e237baad4d69b77d4f5b3c14762e6
Niklas Lundberg <info@nlundberg.se>
2018-05-16 09:26:12 +0200

    Fixed exit code in build script.

--- build/Build.bat
+++ build/Build.bat
@@ -47,3 +47,5 @@ SET Arbor.X.Vcs.Branch.Name.Version.OverrideEnabled=
 SET Arbor.X.VariableOverrideEnabled=
 SET Arbor.X.Artifacts.CleanupBeforeBuildEnabled=
 SET Arbor.X.Build.NetAssembly.Configuration=
+
+EXIT /B %ERRORLEVEL%
