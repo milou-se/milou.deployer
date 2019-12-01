7bac5c2fcad870f18a7b70afbf9f4690d08f3e83
Niklas Lundberg <info@nlundberg.se>
2019-12-01 14:37:31 +0100

    Updated build script.

--- build/Build.bat
+++ build/Build.bat
@@ -1,47 +1,48 @@
 @ECHO OFF
-SET Arbor.X.Build.Bootstrapper.AllowPrerelease=true
-SET Arbor.X.Tools.External.MSpec.Enabled=true
-SET Arbor.X.NuGet.Package.Artifacts.Suffix=
-SET Arbor.X.NuGet.Package.Artifacts.BuildNumber.Enabled=
-SET Arbor.X.NuGetPackageVersion=
-SET Arbor.X.Vcs.Branch.Name.Version.OverrideEnabled=false
-SET Arbor.X.Build.VariableOverrideEnabled=true
-SET Arbor.X.Artifacts.CleanupBeforeBuildEnabled=true
-SET Arbor.X.Build.NetAssembly.Configuration=
-SET Arbor.X.Tools.External.LibZ.Enabled=true
-SET Arbor.X.MSBuild.NuGetRestore.Enabled=true
+SET Arbor.Builld.Build.Bootstrapper.AllowPrerelease=true
+SET Arbor.Builld.Tools.External.MSpec.Enabled=true
+SET Arbor.Builld.NuGet.Package.Artifacts.Suffix=
+SET Arbor.Builld.NuGet.Package.Artifacts.BuildNumber.Enabled=
+SET Arbor.Builld.NuGetPackageVersion=
+SET Arbor.Builld.Vcs.Branch.Name.Version.OverrideEnabled=true
+SET Arbor.Builld.Vcs.Branch.Name=%GITHUB_REF%
+SET Arbor.Builld.Build.VariableOverrideEnabled=true
+SET Arbor.Builld.Artifacts.CleanupBeforeBuildEnabled=true
+SET Arbor.Builld.Build.NetAssembly.Configuration=
+SET Arbor.Builld.Tools.External.LibZ.Enabled=true
+SET Arbor.Builld.MSBuild.NuGetRestore.Enabled=true
 
-SET Arbor.X.NuGet.ReinstallArborPackageEnabled=true
-SET Arbor.X.NuGet.VersionUpdateEnabled=false
-SET Arbor.X.Artifacts.PdbArtifacts.Enabled=true
-SET Arbor.X.NuGet.Package.CreateNuGetWebPackages.Enabled=true
+SET Arbor.Builld.NuGet.ReinstallArborPackageEnabled=true
+SET Arbor.Builld.NuGet.VersionUpdateEnabled=false
+SET Arbor.Builld.Artifacts.PdbArtifacts.Enabled=true
+SET Arbor.Builld.NuGet.Package.CreateNuGetWebPackages.Enabled=true
 
-SET Arbor.X.Build.NetAssembly.MetadataEnabled=true
-SET Arbor.X.Build.NetAssembly.Description=Milou Deployer
-SET Arbor.X.Build.NetAssembly.Company=Milou Communication AB
-SET Arbor.X.Build.NetAssembly.Copyright=(C) Milou Communication AB 2015-2018
-SET Arbor.X.Build.NetAssembly.Trademark=Milou Deployer
-SET Arbor.X.Build.NetAssembly.Product=Milou Deployer
-SET Arbor.X.ShowAvailableVariablesEnabled=false
-SET Arbor.X.ShowDefinedVariablesEnabled=false
-SET Arbor.X.Tools.External.MSBuild.Verbosity=minimal
-SET Arbor.X.NuGet.Package.AllowManifestReWriteEnabled=false
+SET Arbor.Builld.Build.NetAssembly.MetadataEnabled=true
+SET Arbor.Builld.Build.NetAssembly.Description=Milou Deployer
+SET Arbor.Builld.Build.NetAssembly.Company=Milou Communication AB
+SET Arbor.Builld.Build.NetAssembly.Copyright=(C) Milou Communication AB 2015-2018
+SET Arbor.Builld.Build.NetAssembly.Trademark=Milou Deployer
+SET Arbor.Builld.Build.NetAssembly.Product=Milou Deployer
+SET Arbor.Builld.ShowAvailableVariablesEnabled=false
+SET Arbor.Builld.ShowDefinedVariablesEnabled=false
+SET Arbor.Builld.Tools.External.MSBuild.Verbosity=minimal
+SET Arbor.Builld.NuGet.Package.AllowManifestReWriteEnabled=false
 
-SET Arbor.X.NuGet.Package.ExcludesCommaSeparated=Arbor.X.Bootstrapper.nuspec
+SET Arbor.Builld.NuGet.Package.ExcludesCommaSeparated=Arbor.Builld.Bootstrapper.nuspec
 
 CALL dotnet arbor-build
 
 REM Restore variables to default
 
-SET Arbor.X.Build.Bootstrapper.AllowPrerelease=
-SET Arbor.X.Tools.External.MSpec.Enabled=
-SET Arbor.X.NuGet.Package.Artifacts.Suffix=
-SET Arbor.X.NuGet.Package.Artifacts.BuildNumber.Enabled=
-SET Arbor.X.Log.Level=
-SET Arbor.X.NuGetPackageVersion=
-SET Arbor.X.Vcs.Branch.Name.Version.OverrideEnabled=
-SET Arbor.X.VariableOverrideEnabled=
-SET Arbor.X.Artifacts.CleanupBeforeBuildEnabled=
-SET Arbor.X.Build.NetAssembly.Configuration=
+SET Arbor.Builld.Build.Bootstrapper.AllowPrerelease=
+SET Arbor.Builld.Tools.External.MSpec.Enabled=
+SET Arbor.Builld.NuGet.Package.Artifacts.Suffix=
+SET Arbor.Builld.NuGet.Package.Artifacts.BuildNumber.Enabled=
+SET Arbor.Builld.Log.Level=
+SET Arbor.Builld.NuGetPackageVersion=
+SET Arbor.Builld.Vcs.Branch.Name.Version.OverrideEnabled=
+SET Arbor.Builld.VariableOverrideEnabled=
+SET Arbor.Builld.Artifacts.CleanupBeforeBuildEnabled=
+SET Arbor.Builld.Build.NetAssembly.Configuration=
 
 EXIT /B %ERRORLEVEL%
