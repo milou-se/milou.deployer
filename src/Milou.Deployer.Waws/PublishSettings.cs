1bc1db24201c60731e797173ae7db36863fe4c63
Niklas Lundberg <info@nlundberg.se>
2020-03-23 22:28:54 +0100

    Started publish settings loading.

--- src/Milou.Deployer.Waws/PublishSettings.cs
+++ src/Milou.Deployer.Waws/PublishSettings.cs
@@ -1,22 +1,23 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using JetBrains.Annotations;
 
 namespace Milou.Deployer.Waws
 {
     internal class PublishSettings
     {
-        public static async  Task<PublishSettings> Load(string publishSettingsFile)
-        {
-            /*
-             <publishData>
-    <publishProfile profileName="" publishMethod="MSDeploy" publishUrl="scm.azurewebsites.net:443" msdeploySite="" userName="$" userPWD="" destinationAppUrl="" SQLServerDBConnectionString="" mySQLDBConnectionString="" hostingProviderForumLink="" controlPanelLink="" webSystem="WebSites">
-        <databases />
-    </publishProfile>
-             */
-
-            throw new NotImplementedException();
-        }
+        private const string PublishData = "publishData";
+        private const string PublishProfile = "publishProfile";
+        private const string PublishMethod = "publishMethod";
+        private const string MSDeploy = "MSDeploy";
+        private const string PublishUrl = "publishUrl";
+        private const string MSDeploySite = "msdeploySite";
+        private const string UserPwd = "userPWD";
+        private const string UserName = "userName";
 
         [CanBeNull]
         public string SiteName { get; set; }
@@ -30,5 +31,49 @@ namespace Milou.Deployer.Waws
         public AuthenticationType AuthenticationType { get; set; }
 
         public bool AllowUntrusted { get; set; }
+
+        public static async Task<PublishSettings> Load(string publishSettingsFile, CancellationToken cancellationToken)
+        {
+            /*
+             <publishData>
+    <publishProfile profileName="" publishMethod="MSDeploy" publishUrl="scm.azurewebsites.net:443" msdeploySite="" userName="$" userPWD="" destinationAppUrl="" SQLServerDBConnectionString="" mySQLDBConnectionString="" hostingProviderForumLink="" controlPanelLink="" webSystem="WebSites">
+        <databases />
+    </publishProfile>
+             */
+
+
+            var document =
+                await XDocument.LoadAsync(File.OpenRead(publishSettingsFile), LoadOptions.None, cancellationToken);
+
+            var profiles = document?.Element(PublishData)
+                ?.Descendants(PublishProfile).ToArray() ?? Array.Empty<XElement>();
+
+            if (profiles.Length == 0)
+            {
+                throw new InvalidOperationException(
+                    $"Could not find any publish profiles in file '{publishSettingsFile}'");
+            }
+
+            XElement profile;
+
+            if (profiles.Length > 1)
+            {
+                profile =
+                    profiles.FirstOrDefault(
+                        current => current.Attribute(PublishMethod)?.Value.Equals(MSDeploy) ?? false) ?? profiles[0];
+            }
+            else
+            {
+                profile = profiles[0];
+            }
+
+            return new PublishSettings
+            {
+                ComputerName = profile.Attribute(PublishUrl)?.Value,
+                SiteName = profile.Attribute(MSDeploySite)?.Value,
+                Username = profile.Attribute(UserName)?.Value,
+                Password = profile.Attribute(UserPwd)?.Value
+            };
+        }
     }
 }
