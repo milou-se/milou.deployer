c1e4d576a65b76c02223300e0a201f326d04e96b
Niklas Lundberg <info@nlundberg.se>
2020-04-17 22:23:58 +0200

    Started docker builds.

---
+++ build/docker/build-agent.bat
@@ -0,0 +1,34 @@
+@ECHO OFF
+SET BuildTemp=%~dp0AgentTemp
+
+IF EXIST "%BuildTemp%" (
+  ECHO "Removing temp directory '%BuildTemp%'"
+  RD "%BuildTemp%" /s /q
+)
+
+ECHO "Creating directory '%BuildTemp%'"
+mkdir "%BuildTemp%"
+
+CD "%~dp0"
+
+CD ..
+CD ..
+
+SET Artifacts=Artifacts\Websites\Milou.Deployer.Web.IisHost\AnyCPU\debug
+ECHO Copying files from %Artifacts% to %BuildTemp%
+xcopy "%Artifacts%" "%BuildTemp%\data" /e /s
+
+CD "%~dp0"
+
+COPY build\agent\*.* "%BuildTemp%\"
+COPY build\*.* "%BuildTemp%\"
+
+cd "%BuildTemp%"
+
+ECHO "Running docker build in '%CD%'"
+
+CALL docker build .
+
+ECHO "Getting back to script directory"
+
+CD "%~dp0"
