caf2a912bf0a52666b81694f4df67dbadf96e4ca
Niklas Lundberg <info@nlundberg.se>
2020-05-11 18:41:17 +0200

    Separated classes to files.

--- src/Milou.Deployer.Core/Extensions/ObjectExtensions.cs
+++ src/Milou.Deployer.Core/Extensions/ObjectExtensions.cs
@@ -1,20 +1,7 @@
-﻿using System.Collections.Generic;
-
-namespace Milou.Deployer.Core.Extensions
+﻿namespace Milou.Deployer.Core.Extensions
 {
     public static class ObjectExtensions
     {
         public static bool HasValue<T>(this T item) where T : class => item is {};
-    }
-
-    public static class HashSetExtensions
-    {
-        public static void AddRange<T>(this HashSet<T> hashSet, IEnumerable<T> items)
-        {
-            foreach (var item in items)
-            {
-                hashSet.Add(item);
-            }
-        }
     }
 }
